809d7e7e319aa902b2576dfb0220fc096605a324
0QKub0 <qkubplay@gmail.com>
2025-02-16 20:23:46 +0100

    dashboard profile

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,40 @@
+"use client";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { usePathname } from "next/navigation";
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-    const pathName = usePathname();
-    
+import { useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import Loading from "@/components/Loading";
+import { cn } from "@/lib/utils";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/AppSidebar";
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathName = usePathname();
+  const [courseId, setCourseId] = useState<Course | null>(null);
+  const { user, isLoaded } = useUser();
+
+  //handle use effect isCoursePage
+
+  if (!isLoaded) return <Loading />;
+  if (!user) return <div>Please sign in to access this page.</div>;
   return (
-    <div className="nondashboard-layout">
-      <main className="nondashboard-layout__main">{children}</main>
-    </div>
+    <SidebarProvider>
+      <div className="dashboard">
+        <AppSidebar />
+        <div className="dashboard__content">
+          {/* chapter sidebar will go  */}
+          <div
+            className={cn("dashboard__main")}
+            style={{ height: "100vh" }}
+          ></div>
+          <Navbar />
+          <main className="dashboard__body">{children}</main>
+        </div>
+      </div>
+    </SidebarProvider>
   );
 }
